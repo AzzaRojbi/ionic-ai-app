a24a6b6b50d9d0a083e70a8c57c64e71bfd81083
Azza Rojbi <arojbi@my.bcit.ca>
2017-12-08 02:23:17 -0800

    setting up cordova and dialogflow.

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, NgZone } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Platform } from 'ionic-angular';
+
+declare const ApiAIPromises: any;
 
 @Component({
   selector: 'page-home',
@@ -7,8 +10,29 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  answer;  
 
+  constructor(public navCtrl: NavController, public platform: Platform, public ngZone: NgZone) {
+    platform.ready().then(() => {
+
+      ApiAIPromises.init({
+        clientAccessToken: "c2f84979143a48519f2210714a2fbbff"
+      })
+      .then((result) =>  console.log(result))
+
+    });
+  }
+
+
+  ask(question) {
+    ApiAIPromises.requestText({
+      query: question
+    })
+    .then(({result: {fulfillment: {speech}}}) => {
+       this.ngZone.run(()=> {
+         this.answer = speech;
+       });
+    })
   }
 
 }
